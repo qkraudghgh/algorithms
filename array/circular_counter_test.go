595879ebbb344c9af72c48e8e9f2e22c077984f4
myoungho.pak <qkraudghgh@gmail.com>
2017-04-27 20:11:28 +0900

    Improve Testing log in circular_counter

--- array/circular_counter_test.go
+++ array/circular_counter_test.go
@@ -8,6 +8,7 @@ import (
 func TestJosepheusFunc(t *testing.T) {
 	t.Log("Test Circular Counter algorithm")
 	temp := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println("INPUT: [1, 2, 3, 4, 5, 6, 7, 8, 9]")
 	fmt.Print("OUTPUT: ")
 	josepheus(temp, 3)
 	fmt.Println()
