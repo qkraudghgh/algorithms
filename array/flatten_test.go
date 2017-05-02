a103ac76d242ed0ccabbdd714ed064f500caaa3e
myoungho.pak <qkraudghgh@gmail.com>
2017-05-02 16:21:17 +0900

    Add flatten algorithm

--- array/flatten_test.go
+++ array/flatten_test.go
@@ -1,1 +1,13 @@
 package array
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFlattenFunc(t *testing.T) {
+	t.Log("Test Flatten algorithm")
+	target := any{2, 1, any{3, any{4, 5}, 6}, 7, any{8}}
+	fmt.Println("INPUT: ", target)
+	fmt.Println("OUTPUT: ", flatten(target, any{}))
+}
